15de171ca4f05c418ed81cb028614228702f0b83
Frank Storbeck <frank@foef.nl>
2022-04-07 14:02:04 +0200

    Added Set() and Get() for nodes

--- node.go
+++ node.go
@@ -12,3 +12,13 @@ type Node struct {
 func NewNode(data interface{}) *Node {
 	return &Node{data: data, siblings: make([]*Node, 0)}
 }
+
+// Get returns the stored data.
+func (nd *Node) Get() interface{} {
+	return nd.data
+}
+
+// Set stores the data
+func (nd *Node) Set(data interface{}) {
+	nd.data = data
+}
--- node_test.go
+++ node_test.go
@@ -1,11 +1,34 @@
 package otree
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestNewNode(t *testing.T) {
 	s0 := "s0"
 	nd := NewNode(s0)
 	if got := nd.data.(string); got != s0 {
-		t.Errorf("NewNode(%q) returs data %q, should be %q", s0, got, s0)
+		t.Errorf("NewNode(%q) returns data %q, should be %q", s0, got, s0)
+	}
+}
+
+func TestSetGet(t *testing.T) {
+	tests := []struct {
+		data interface{}
+	}{
+		{"s"},
+		{8},
+		{struct {
+			i int
+			f float32
+		}{2, 3.4}},
+	}
+	nd := NewNode("")
+	for _, tst := range tests {
+		nd.Set(tst.data)
+		got := nd.Get()
+		if got != tst.data {
+			t.Errorf("Get() after Set(%v) returns %v, should be %v", tst.data, got, tst.data)
+		}
 	}
 }
